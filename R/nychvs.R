8c78fe8197c6ec8785fb5f60201881df9e4930ed
Nikola Baci <46806109+nikolabaci98@users.noreply.github.com>
2023-09-15 10:44:39 -0400

    Update nychvs.R

--- R/nychvs.R
+++ R/nychvs.R
@@ -154,6 +154,54 @@ lodown_nychvs <-
 
 	}
 
+uncomment <- function( SASinput , starting.comment , ending.comment ){
+
+	#remove /* */
+	for ( i in 1:length(SASinput) ){
+		
+		#test if the line contains a slash_asterisk (or any opening comment character)
+		slash_asterisk <- unlist( gregexpr( starting.comment , SASinput[ i ] , fixed = T ) )
+		
+		#test if the line contains a asterisk_slash (or any closing comment character)
+		asterisk_slash <- unlist( gregexpr( ending.comment , SASinput[ i ] , fixed = T ) )
+		
+		#only if the line contains an slash_asterisk..
+		if ( !( -1 %in% slash_asterisk ) ){
+			
+			#if there's a closing asterisk_slash on that line..
+			if ( max( asterisk_slash ) > min( slash_asterisk ) ){
+				
+				SASinput[ i ] <- sub( substr( SASinput[ i ] , slash_asterisk[1] , asterisk_slash[1] + 1 ) , "" , SASinput[ i ] , fixed = T )
+				
+				#and re-do the line just in case there's more than one comment!!
+				i <- i - 1
+				
+			} else {
+			
+				#delete the rest of the line
+				SASinput[ i ] <- sub( substr( SASinput[ i ] , slash_asterisk[1] , nchar( SASinput[ i ] ) ) , "" , SASinput[ i ] , fixed = T )
+				
+				#start a new counter
+				j <- i
+				
+				#keep going until you find a asterisk_slash
+				while( max( asterisk_slash ) < 0 ){
+					j <- j + 1
+					
+					#look for asterisk_slash again
+					asterisk_slash <- unlist( gregexpr( ending.comment , SASinput[ j ] , fixed = T ) )
+					
+					#if the asterisk_slash doesn't exist, delete the whole line
+					if ( max( asterisk_slash ) < 0 ) SASinput[ j ] <- ""
+					#otherwise delete until the asterisk_slash
+					else SASinput[ j ] <- sub( substr( SASinput[ j ] , 1 , asterisk_slash[1] + 1 ) , "" , SASinput[ j ] , fixed = T )
+				}
+			}
+		}
+	}
+
+	SASinput
+}
 
 
 # initiate a function that will clean up doubly-embedded /* */ - which are allowed in SAS but not SAScii
@@ -171,7 +219,7 @@ nychvs_sas_cleanup <-
 		y <- gsub( "@514            uf39a           $7." , "@514            uf39a           $1." , y , fixed = TRUE )
 		
 		# also, while we're removing stuff we don't like, throw out `TAB` characters
-		z <- gsub( "\t" , " " , SAScii::SAS.uncomment( SAScii::SAS.uncomment( y , "/*" , "*/" ) , "/*" , "*/" ) )
+		z <- gsub( "\t" , " " , uncomment(uncomment( y , "/*" , "*/" ) , "/*" , "*/" ) )
 		
 		# get rid of this crap
 		z <- gsub( "comma([0-9])\\.([0-9])" , "\\1.\\2" , z )
